17a06e7712c3a569dfac74e11caa2385b3e736fa
Franceschini-Bambou <bambou.franceschini@edu.univ-fcomte.fr>
2025-11-07 15:51:47 +0100

    TP1 - simples interactions - Franceschini Bambou

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 // TODO
+const aMettreEnrouge = document.querySelector('#a-mettre-en-rouge')
+aMettreEnrouge.style.color="red"
+
+const enRougeSuiteAclick = document.querySelector('#en-rouge-suite-a-click')
+enRougeSuiteAclick.addEventListener ("click", (evt)=>{
+    enRougeSuiteAclick.style.color='red'
+});
+
+const collH2 = document.querySelectorAll ("h2")
+collH2.forEach ((elm)=>{
+    elm.addEventListener('click',(evt)=>{ 
+        evt.target.style.color = "red";
+});
+});
